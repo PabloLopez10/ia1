a8dc06e61cbbe55f652d95820f6f7d0926f34d0e
Pablo Lopez <>
2019-02-21 12:38:30 -0600

    Despliegue de tablero inicial

--- sudoku.py
+++ sudoku.py
@@ -2,7 +2,7 @@ import sys
 
 entrada = sys.argv[1]
 aceptados = [".","1","2","3","4"]
-
+error = False
 
 while(True):
 	if(len(sys.argv[1])==22):
@@ -14,7 +14,16 @@ while(True):
 					error = True
 					break
 			if(error != True):
-				print(inicial)
+				print("El tablero del sudoku es: ")
+				print(" ----------------")
+				print("| "+inicial[0]+" | "+inicial[1]+" || "+inicial[2]+" | "+inicial[3]+" |")
+				print(" ----------------")
+				print("| "+inicial[4]+" | "+inicial[5]+" || "+inicial[6]+" | "+inicial[7]+" |")
+				print(" ----------------")
+				print("| "+inicial[8]+" | "+inicial[9]+" || "+inicial[10]+" | "+inicial[11]+" |")
+				print(" ----------------")
+				print("| "+inicial[12]+" | "+inicial[13]+" || "+inicial[14]+" | "+inicial[15]+" |")
+				print(" ----------------")
 				break
 			else:
 				print("Ingreso caracter invalido")
