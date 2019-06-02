0981b7794a3a6fd86759c6a7667c26f0997c9dd5
PabloLopez10 <lop14509@uvg.edu.gt>
2019-06-02 13:05:46 -0600

    Add files via upload
    sudoku final

--- sudoku.py
+++ sudoku.py
@@ -1,16 +1,16 @@
 #Pablo Lopez 14509
-#Inteligencia Artificial 
-
-import sys
-from random import randint
+#Inteligencia artificial
+#Proyecto 1
 from collections import OrderedDict
+import sys
 
-#Globales
-header = sys.argv[1][:6]
-inicial = sys.argv[1][6:]
-aceptados = [".","1","2","3","4"]
-sudoku = [0 if x=="." else int(x)for x in inicial]
-proto_sudoku = sudoku[:]
+#variables globales
+
+Entrada = ""
+Entrada = sys.argv[1]
+inicial = ""
+L = list(Entrada)
+sudoku = [0 if x=="." else int(x)for x in Entrada]
 frontier = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
 values_present = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
 cleaner = []
@@ -22,60 +22,224 @@ ordered_actions = {}
 action_keys = []
 visitados = {}
 full_counter = 0
-error = False
 
-#Columnas
+
+def tablero(entrada):
+    print(" -*-*-*-*-*-*-*-*-")
+    print("| "+entrada[0]+" | "+entrada[1]+" || "+entrada[2]+" | "+entrada[3]+" |")
+    print(" ----------------")
+    print("| "+entrada[4]+" | "+entrada[5]+" || "+entrada[6]+" | "+entrada[7]+" |")
+    print(" -*-*-*-*-*-*-*-*-")
+    print("| "+entrada[8]+" | "+entrada[9]+" || "+entrada[10]+" | "+entrada[11]+" |")
+    print(" ----------------")
+    print("| "+entrada[12]+" | "+entrada[13]+" || "+entrada[14]+" | "+entrada[15]+" |")
+    print(" -*-*-*-*-*-*-*-*-")
+
+
+
+#rows
+row1 = []
+row2 = []
+row3 = []
+row4 = []
+def fill_rows():
+    global row1,row2,row3,row4,sudoku
+    row1 = sudoku[0:4]
+    row2 = sudoku[4:8]
+    row3 = sudoku[8:12]
+    row4 = sudoku[12:16]
+    
+#cols
 col1 = []
 col2 = []
 col3 = []
 col4 = []
 
-#Filas
-row1 = []
-row2 = []
-row3 = []
-row4 = []
+def fill_cols():
+    global col1,col2,col3,col4,sudoku
+    
+    col1 = (sudoku[0:1]+ sudoku[4:5] + sudoku[8:9]+ sudoku[12:13])
+    col2 = (sudoku[1:2]+ sudoku[5:6] + sudoku[9:10]+ sudoku[13:14])
+    col3 = (sudoku[2:3]+ sudoku[6:7] + sudoku[10:11]+ sudoku[14:15])
+    col4 = (sudoku[3:4]+ sudoku[7:8] + sudoku[11:12]+ sudoku[15:16])
+        
 
-def actions2(entrada):
-	b=0
-	for x in entrada:
-		if(x == "."): 
-			a = str(randint(1,4))
-			entrada = entrada[0:b] + a + entrada[b+1:]
-		b=b+1
-	return entrada
+#nodos
 
-def tablero(entrada):
-	print(" -*-*-*-*-*-*-*-*-")
-	print("| "+entrada[0]+" | "+entrada[1]+" || "+entrada[2]+" | "+entrada[3]+" |")
-	print(" ----------------")
-	print("| "+entrada[4]+" | "+entrada[5]+" || "+entrada[6]+" | "+entrada[7]+" |")
-	print(" -*-*-*-*-*-*-*-*-")
-	print("| "+entrada[8]+" | "+entrada[9]+" || "+entrada[10]+" | "+entrada[11]+" |")
-	print(" ----------------")
-	print("| "+entrada[12]+" | "+entrada[13]+" || "+entrada[14]+" | "+entrada[15]+" |")
-	print(" -*-*-*-*-*-*-*-*-")
+node1 =  []
+node2 =  []
+node3 =  []
+node4 =  []
+node5 =  []
+node6 =  []
+node7 =  []
+node8 =  []
+node9 =  []
+node10 = []
+node11 = []
+node12 = []
+node13 = []
+node14 = []
+node15 = []
+node16 = []
 
-while(True):
-	if(len(sys.argv[1])==22):
-		if(header == "start="):
-			for i in inicial:
-				if(i not in aceptados):
-					error = True
-					break
-			if(error != True):
-				print("El tablero inicial es: ")
-				tablero(inicial)
-				inicial = actions2(inicial)
-				print("El tablero final es: ")
-				tablero(inicial)
-				break
-			else:
-				print("Ingreso caracter invalido")
-				break
-		else:
-			print("Encabezado incorrecto")
-			break
-	else:
-		print("Entrada incorrecta")
-		break
+def create_update_nodes():
+    global node1,node2,node3,node4,node5,node6,node7,node8,node9,node10,node11,node12,node13,node14,node15,node16,nodes
+    
+    node1.append(row1 + col1[1:] + col2[1:2])
+    node2.append(row1 + col2[1:] + col1[1:2])
+    node3.append(row1 + col3[1:] + col4[1:2])
+    node4.append(row1 + col4[1:] + col3[1:2])
+
+    #el skip no se ve muy bien pero se ordena de la manera mas facil de asegurarse que estan todos los numeros
+    node5.append(row2 + col1[0:1] + col1[2:] + row1[1:2])
+    node6.append(row2 + col2[0:1] + col2[2:] + row1[0:1])
+    node7.append(row2 + col3[0:1] + col3[2:] + col4[0:1])
+    node8.append(row2 + col4[0:1] + col4[2:] + col3[0:1])
+
+    #3er fila
+    node9.append( row3 + col1[0:2] + col1[3:] + row4[1:2])
+    node10.append(row3 + col2[0:2] + col2[3:] + row4[0:1])
+    node11.append(row3 + col3[0:2] + col3[3:] + row4[3:4])
+    node12.append(row3 + col4[0:2] + col4[3:] + row4[2:3])
+
+    #4a fila
+    #ya no hay skip como la primera fila
+    node13.append(row4 + col1[:3] + row3[1:2])
+    node14.append(row4 + col2[:3] + row3[0:1])
+    node15.append(row4 + col3[:3] + row3[3:4])
+    node16.append(row4 + col4[:3] + row3[2:3])
+
+    nodes[0]  = node1
+    nodes[1]  = node2
+    nodes[2]  = node3
+    nodes[3]  = node4
+    nodes[4]  = node5
+    nodes[5]  = node6
+    nodes[6]  = node7
+    nodes[7]  = node8
+    nodes[8]  = node9
+    nodes[9]  = node10
+    nodes[10] = node11
+    nodes[11] = node12
+    nodes[12] = node13
+    nodes[13] = node14
+    nodes[14] = node15
+    nodes[15] = node16
+
+
+#frontera
+def fill_frontier():
+    global frontier, nodes, values_present
+    for i in range(16):    
+        for value in nodes[i]:
+            if value not in values_present[i] and value != 0:
+                values_present[i].append(value)
+                frontier[i].append(list(set(possible_values)-set(values_present[i][0])))
+
+#print(frontier)
+def fill_actions():
+    global sudoku,actions,frontier
+    for i in range(16):
+        if(sudoku[i]==0):
+            actions[i] = frontier[i][0]
+
+#ordenar de menor a mayor las acciones
+def actionkeys():
+    global action_keys,ordered_actions
+    ordered_actions = OrderedDict(sorted(actions.items(), key = lambda item: len(item[1]),reverse = False))
+    action_keys = list(ordered_actions.keys())
+
+
+#print(action_keys)
+#print(ordered_actions)
+#eliminar opciones con 1 posibilidad
+#resolver sudoku
+def solve():
+    global action_keys,ordered_actions,sudoku
+    for i in range(len(action_keys)):
+        if(len(ordered_actions[action_keys[i]]) ==1):
+            sudoku[action_keys[i]] = ordered_actions[action_keys[i]].pop()
+        #elif(len(ordered_actions[action_keys[i]])>1):
+         #   sudoku[action_keys[i]] = ordered_actions[action_keys[i]][randint(0,len(ordered_actions[action_keys[i]])-1)]
+
+
+while(0 in sudoku and full_counter<16):
+    #llenar filas columnas y nodos
+    fill_rows()
+    fill_cols()
+    create_update_nodes()
+    #frontier
+    fill_frontier()
+    #actions
+    fill_actions()
+    #keys
+    actionkeys()
+    solve()
+    #print(sudoku)
+    inicial = ''.join(str(e) for e in sudoku)
+    #print("| "+sudoku[0]+" | ")
+    #print("| "+inicial[0]+" | ")
+    #tablero(inicial)
+    #terminan las acciones de llenado
+    #vaciar
+    #vaciar diccionario nodos
+    nodes.clear()
+    #vaciar nodos
+    node1.clear()
+    node2.clear()
+    node3.clear()
+    node4.clear()
+    node5.clear()
+    node6.clear()
+    node7.clear()
+    node8.clear()
+    node9.clear()
+    node10.clear()
+    node11.clear()
+    node12.clear()
+    node13.clear()
+    node14.clear()
+    node15.clear()
+    node16.clear()
+
+    #vaciar frontera y acciones y values_present
+    values_present.clear()
+    frontier.clear()
+    actions.clear()
+    for i in range(16):
+        frontier.append([])
+        values_present.append([])
+
+    #vaciar action_keys y ordered actions
+    action_keys.clear()
+    ordered_actions.clear()
+    full_counter += 1
+tablero(inicial)
+#fin del while
+contador = 0
+if(0 in sudoku):
+    print("no es posible resolver este sudoku")
+for i in range(16):
+    if Entrada[i] == ".":
+        contador = contador + 1
+if contador == 0:
+    print("Ingreso un sudoku lleno")
+
+#llenar filas columnas y nodos
+#fill_rows()
+#fill_cols()
+#create_update_nodes()
+
+#frontier
+#fill_frontier()
+#print(frontier)
+#actions
+#fill_actions()
+#keys
+#actionkeys()
+#print(action_keys)
+#print(ordered_actions)
+#solve()
+#print(sudoku)
+
